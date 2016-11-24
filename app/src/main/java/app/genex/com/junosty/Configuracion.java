71ec69bc21e813b0d29bb43122e38cd52c591348
alangamboa97 <alangam97@gmail.com>
2016-11-23 23:59:21 -0600

    Hay un problema con muchas actividadesgit add -A! despues de subir imagen

--- app/src/main/java/app/genex/com/junosty/Configuracion.java
+++ app/src/main/java/app/genex/com/junosty/Configuracion.java
@@ -1,15 +1,24 @@
 package app.genex.com.junosty;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -17,6 +26,11 @@ public class Configuracion extends AppCompatActivity {
     private ImageButton mSetupImagebtn;
     private EditText mNombre;
     private Button  mSubmitbtn;
+    private Uri mImageUri = null;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private StorageReference mStorageImage;
+    private ProgressDialog mProgress;
 
     private static final int GALLERY_REQUEST = 1;
 
@@ -28,6 +42,11 @@ public class Configuracion extends AppCompatActivity {
         mSetupImagebtn = (ImageButton) findViewById(R.id.setupimageButton);
         mNombre = (EditText) findViewById(R.id.editText);
         mSubmitbtn =(Button) findViewById(R.id.ssetupsubmitbutton);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
+        mStorageImage = FirebaseStorage.getInstance().getReference().child("Imagenes");
+
+        mProgress = new ProgressDialog(this);
 
         mSetupImagebtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +59,56 @@ public class Configuracion extends AppCompatActivity {
             }
         });
 
+        mSubmitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSetupAccount();
+            }
+        });
 
+
+
+
+    }
+
+    private void startSetupAccount() {
+
+        final String Nombre = mNombre.getText().toString().trim();
+       final String user_id = mAuth.getCurrentUser().getUid();
+
+
+        if(!TextUtils.isEmpty(Nombre) && mImageUri != null){
+
+            mProgress.setMessage("Espera...");
+            mProgress.show();
+
+            StorageReference filepad = mStorageImage.child(mImageUri.getLastPathSegment());
+
+            filepad.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    String downloadUri = taskSnapshot.getDownloadUrl().toString();
+
+                    mDatabase.child(user_id).child("Nombre").setValue(Nombre);
+                    mDatabase.child(user_id).child("Imagen").setValue(downloadUri);
+
+                    mProgress.dismiss();
+
+
+
+
+                }
+            });
+
+
+
+
+
+
+
+
+        }
 
 
     }
@@ -65,8 +133,8 @@ public class Configuracion extends AppCompatActivity {
             if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
                 CropImage.ActivityResult result = CropImage.getActivityResult(data);
                 if (resultCode == RESULT_OK) {
-                    Uri resultUri = result.getUri();
-                    mSetupImagebtn.setImageURI(resultUri);
+                    mImageUri = result.getUri();
+                    mSetupImagebtn.setImageURI(mImageUri);
 
                 } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                     Exception error = result.getError();
